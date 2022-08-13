fca515a5956afa739364f988a53ff4ba403aaf64
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-08-13 16:28:21 -0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from datetime import datetime
+
+
+class Post(models.Model):
+    autor = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=255)
+    subtitulo = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    data_publicacao = models.DateTimeField(default=datetime.now())
+
+    def __str__(self):
+        return self.titulo
+
