817484dc2e15841b447a674d4198ab9f80f66a32
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-08-14 14:28:58 -0300

    boostrap - mobile first and breakpoints

--- exemplos/views.py
+++ exemplos/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def get_bootstap(request):
 
-    return render(request, 'exemplo/02_container.html')
+    return render(request, 'exemplo/03_breakpoints.html')
