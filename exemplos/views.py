25e2b9b22541422a1db3d694acd8b65bada5f0c4
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-08-15 21:46:33 -0300

    criando esqueleto da Home

--- exemplos/views.py
+++ exemplos/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def get_bootstap(request):
-    print("chegou aqui")
-    return render(request, 'exemplo/05_grid.html')
+    return render(request, 'exemplo/12_utilitarios.html')
 
