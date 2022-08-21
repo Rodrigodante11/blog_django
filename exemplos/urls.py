c019534486d648f75313d400bfe066ed508ab194
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-08-21 17:31:23 -0300

    exemplos de forms

--- exemplos/urls.py
+++ exemplos/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.get_bootstap, name="get_bootstap"),
-
+    path('', views.get_bootstrap, name="get_bootstrap"),
+    path('processa_formulario_v1', views.processa_formulario_v1, name="processa_formulario_v1"),
+    path('processa_formulario_v2', views.processa_formulario_v2, name="processa_formulario_v2"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
