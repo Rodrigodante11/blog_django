99f0e46d38ceaaecf2b6f2f1bb2335d77db8fee1
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-08-25 23:14:32 -0300

    mensagem de feed back e captura de dados do contato.html

--- contato/views.py
+++ contato/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 from contato.contato_form import formContato
 
 
@@ -8,4 +8,14 @@ def contato(request):
 
 
 def processa_contato(request):
+    if request.method == 'POST':
+        contato = formContato(request.POST)
+        if contato.is_valid():
+            assunto = contato.cleaned_data['assunto']
+            messages.success(request, 'Mensagem encaminha com sucess')
+            messages.error(request, 'Mensagem Nao Enviada')
+            return render(request, 'contato/contato.html', {'form': formContato()})
+        else:
+            messages.error(request, 'Mensagem Nao Enviada')
+            return render(request, 'contato/contato.html', {'form': contato})
     return render(request, 'contato/contato.html', {'form': formContato()})
